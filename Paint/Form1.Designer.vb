277a7836aed309066444c4e3fb825e0340bf9180
1030shruti <shruti.d.mandrekar@gmail.com>
2017-08-04 11:15:36 -0400

    Initial commit

---
+++ Paint/Form1.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Red = New System.Windows.Forms.Button()
+        Me.Blue = New System.Windows.Forms.Button()
+        Me.Green = New System.Windows.Forms.Button()
+        Me.Purple = New System.Windows.Forms.Button()
+        Me.Pink = New System.Windows.Forms.Button()
+        Me.Brown = New System.Windows.Forms.Button()
+        Me.Black = New System.Windows.Forms.Button()
+        Me.LightGreen = New System.Windows.Forms.Button()
+        Me.LightBlue = New System.Windows.Forms.Button()
+        Me.LightPurple = New System.Windows.Forms.Button()
+        Me.LightPink = New System.Windows.Forms.Button()
+        Me.Eraser = New System.Windows.Forms.Button()
+        Me.BrshSize = New System.Windows.Forms.HScrollBar()
+        Me.Yellow = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Red
+        '
+        Me.Red.Location = New System.Drawing.Point(12, 12)
+        Me.Red.Name = "Red"
+        Me.Red.Size = New System.Drawing.Size(75, 23)
+        Me.Red.TabIndex = 0
+        Me.Red.Text = "Red"
+        Me.Red.UseVisualStyleBackColor = True
+        '
+        'Blue
+        '
+        Me.Blue.Location = New System.Drawing.Point(93, 11)
+        Me.Blue.Name = "Blue"
+        Me.Blue.Size = New System.Drawing.Size(75, 23)
+        Me.Blue.TabIndex = 1
+        Me.Blue.Text = "Blue"
+        Me.Blue.UseVisualStyleBackColor = True
+        '
+        'Green
+        '
+        Me.Green.Location = New System.Drawing.Point(174, 11)
+        Me.Green.Name = "Green"
+        Me.Green.Size = New System.Drawing.Size(75, 23)
+        Me.Green.TabIndex = 2
+        Me.Green.Text = "Green"
+        Me.Green.UseVisualStyleBackColor = True
+        '
+        'Purple
+        '
+        Me.Purple.Location = New System.Drawing.Point(255, 12)
+        Me.Purple.Name = "Purple"
+        Me.Purple.Size = New System.Drawing.Size(75, 23)
+        Me.Purple.TabIndex = 3
+        Me.Purple.Text = "Purple"
+        Me.Purple.UseVisualStyleBackColor = True
+        '
+        'Pink
+        '
+        Me.Pink.Location = New System.Drawing.Point(336, 12)
+        Me.Pink.Name = "Pink"
+        Me.Pink.Size = New System.Drawing.Size(75, 23)
+        Me.Pink.TabIndex = 4
+        Me.Pink.Text = "Pink"
+        Me.Pink.UseVisualStyleBackColor = True
+        '
+        'Brown
+        '
+        Me.Brown.Location = New System.Drawing.Point(417, 11)
+        Me.Brown.Name = "Brown"
+        Me.Brown.Size = New System.Drawing.Size(75, 23)
+        Me.Brown.TabIndex = 5
+        Me.Brown.Text = "Brown"
+        Me.Brown.UseVisualStyleBackColor = True
+        '
+        'Black
+        '
+        Me.Black.Location = New System.Drawing.Point(498, 12)
+        Me.Black.Name = "Black"
+        Me.Black.Size = New System.Drawing.Size(75, 23)
+        Me.Black.TabIndex = 6
+        Me.Black.Text = "Black"
+        Me.Black.UseVisualStyleBackColor = True
+        '
+        'LightGreen
+        '
+        Me.LightGreen.Location = New System.Drawing.Point(579, 12)
+        Me.LightGreen.Name = "LightGreen"
+        Me.LightGreen.Size = New System.Drawing.Size(75, 23)
+        Me.LightGreen.TabIndex = 7
+        Me.LightGreen.Text = "Light Green"
+        Me.LightGreen.UseVisualStyleBackColor = True
+        '
+        'LightBlue
+        '
+        Me.LightBlue.Location = New System.Drawing.Point(660, 12)
+        Me.LightBlue.Name = "LightBlue"
+        Me.LightBlue.Size = New System.Drawing.Size(75, 23)
+        Me.LightBlue.TabIndex = 8
+        Me.LightBlue.Text = "Light Blue"
+        Me.LightBlue.UseVisualStyleBackColor = True
+        '
+        'LightPurple
+        '
+        Me.LightPurple.Location = New System.Drawing.Point(741, 11)
+        Me.LightPurple.Name = "LightPurple"
+        Me.LightPurple.Size = New System.Drawing.Size(75, 23)
+        Me.LightPurple.TabIndex = 9
+        Me.LightPurple.Text = "Light Purple"
+        Me.LightPurple.UseVisualStyleBackColor = True
+        '
+        'LightPink
+        '
+        Me.LightPink.Location = New System.Drawing.Point(822, 12)
+        Me.LightPink.Name = "LightPink"
+        Me.LightPink.Size = New System.Drawing.Size(75, 23)
+        Me.LightPink.TabIndex = 10
+        Me.LightPink.Text = "Light Pink"
+        Me.LightPink.UseVisualStyleBackColor = True
+        '
+        'Eraser
+        '
+        Me.Eraser.Location = New System.Drawing.Point(903, 13)
+        Me.Eraser.Name = "Eraser"
+        Me.Eraser.Size = New System.Drawing.Size(75, 22)
+        Me.Eraser.TabIndex = 11
+        Me.Eraser.Text = "Eraser"
+        Me.Eraser.UseVisualStyleBackColor = True
+        '
+        'BrshSize
+        '
+        Me.BrshSize.Location = New System.Drawing.Point(1061, 17)
+        Me.BrshSize.Minimum = 10
+        Me.BrshSize.Name = "BrshSize"
+        Me.BrshSize.Size = New System.Drawing.Size(73, 18)
+        Me.BrshSize.TabIndex = 12
+        Me.BrshSize.Value = 10
+        '
+        'Yellow
+        '
+        Me.Yellow.Location = New System.Drawing.Point(984, 14)
+        Me.Yellow.Name = "Yellow"
+        Me.Yellow.Size = New System.Drawing.Size(78, 20)
+        Me.Yellow.TabIndex = 13
+        Me.Yellow.Text = "Yellow"
+        Me.Yellow.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Window
+        Me.ClientSize = New System.Drawing.Size(1153, 599)
+        Me.Controls.Add(Me.Yellow)
+        Me.Controls.Add(Me.BrshSize)
+        Me.Controls.Add(Me.Eraser)
+        Me.Controls.Add(Me.LightPink)
+        Me.Controls.Add(Me.LightPurple)
+        Me.Controls.Add(Me.LightBlue)
+        Me.Controls.Add(Me.LightGreen)
+        Me.Controls.Add(Me.Black)
+        Me.Controls.Add(Me.Brown)
+        Me.Controls.Add(Me.Pink)
+        Me.Controls.Add(Me.Purple)
+        Me.Controls.Add(Me.Green)
+        Me.Controls.Add(Me.Blue)
+        Me.Controls.Add(Me.Red)
+        Me.MinimumSize = New System.Drawing.Size(1169, 638)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Red As Button
+    Friend WithEvents Blue As Button
+    Friend WithEvents Green As Button
+    Friend WithEvents Purple As Button
+    Friend WithEvents Pink As Button
+    Friend WithEvents Brown As Button
+    Friend WithEvents Black As Button
+    Friend WithEvents LightGreen As Button
+    Friend WithEvents LightBlue As Button
+    Friend WithEvents LightPurple As Button
+    Friend WithEvents LightPink As Button
+    Friend WithEvents Eraser As Button
+    Friend WithEvents BrshSize As HScrollBar
+    Friend WithEvents Yellow As Button
+End Class
---
+++ Paint/Form1.vb
@@ -0,0 +1,123 @@
+﻿Public Class Form1
+    Dim myBrush As New SolidBrush(Color.Black)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Location = New Point(0, 0)
+    End Sub
+
+
+    'Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    '    Dim draw As System.Drawing.Graphics
+    '    draw = Me.CreateGraphics
+    '    Dim bluePen As New Pen(Color.Blue, 3)
+    '    Dim redPen As New Pen(Color.Red, 3)
+
+    '    draw.DrawEllipse(bluePen, 80, 200, 100, 100)
+    '    draw.DrawLine(bluePen, 130, 300, 130, 400)
+    '    draw.DrawLine(bluePen, 130, 400, 180, 320)
+    '    draw.DrawLine(bluePen, 130, 400, 80, 320)
+    '    draw.DrawLine(bluePen, 130, 400, 190, 530)
+    '    draw.DrawLine(bluePen, 130, 400, 70, 530)
+    '    draw.DrawEllipse(redPen, 300, 200, 100, 100)
+    '    draw.DrawLine(redPen, 350, 300, 350, 400)
+    '    draw.DrawLine(redPen, 350, 400, 300, 340)
+    '    draw.DrawLine(redPen, 350, 400, 400, 350)
+    '    draw.DrawLine(redPen, 350, 400, 410, 530)
+    '    draw.DrawLine(redPen, 350, 400, 280, 530)
+    'End Sub
+
+    'Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+    '    Label1.Text = MousePosition.X
+    '    Label2.Text = MousePosition.Y
+    'End Sub
+    Dim d As Boolean = False
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Dim draw As System.Drawing.Graphics
+        draw = Me.CreateGraphics
+        Dim X As Integer = Form1.MousePosition.X - Me.Location.X - 10
+        Dim Y As Integer = Form1.MousePosition.Y - Me.Location.Y - 35
+
+        If d = True Then
+            draw.FillEllipse(myBrush, X, Y, BrshSize.Value, BrshSize.Value)
+        End If
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+
+        Dim draw As System.Drawing.Graphics
+        draw = Me.CreateGraphics
+        d = True
+
+        Dim X As Integer = Form1.MousePosition.X - Me.Location.X - 10
+        Dim Y As Integer = Form1.MousePosition.Y - Me.Location.Y - 35
+
+
+
+
+
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
+
+
+        d = False
+
+
+    End Sub
+
+    Private Sub Red_Click(sender As Object, e As EventArgs) Handles Red.Click
+        myBrush.Color = Color.Red
+    End Sub
+
+    Private Sub Blue_Click(sender As Object, e As EventArgs) Handles Blue.Click
+        myBrush.Color = Color.Blue
+    End Sub
+
+    Private Sub Green_Click(sender As Object, e As EventArgs) Handles Green.Click
+        myBrush.Color = Color.Green
+    End Sub
+
+    Private Sub Purple_Click(sender As Object, e As EventArgs) Handles Purple.Click
+        myBrush.Color = Color.Purple
+    End Sub
+
+    Private Sub Pink_Click(sender As Object, e As EventArgs) Handles Pink.Click
+        myBrush.Color = Color.LightCoral
+    End Sub
+
+    Private Sub Brown_Click(sender As Object, e As EventArgs) Handles Brown.Click
+        myBrush.Color = Color.Brown
+    End Sub
+
+    Private Sub Black_Click(sender As Object, e As EventArgs) Handles Black.Click
+        myBrush.Color = Color.Black
+    End Sub
+
+    Private Sub LightGreen_Click(sender As Object, e As EventArgs) Handles LightGreen.Click
+        myBrush.Color = Color.LightGreen
+    End Sub
+
+    Private Sub LightBlue_Click(sender As Object, e As EventArgs) Handles LightBlue.Click
+        myBrush.Color = Color.LightSkyBlue
+    End Sub
+
+    Private Sub LightPurple_Click(sender As Object, e As EventArgs) Handles LightPurple.Click
+        myBrush.Color = Color.Lavender
+    End Sub
+
+    Private Sub LightPink_Click(sender As Object, e As EventArgs) Handles LightPink.Click
+        myBrush.Color = Color.Pink
+    End Sub
+
+    Private Sub Eraser_Click(sender As Object, e As EventArgs) Handles Eraser.Click
+        myBrush.Color = Color.White
+    End Sub
+
+    Private Sub HScrollBar1_Scroll(sender As Object, e As ScrollEventArgs) Handles BrshSize.Scroll
+
+    End Sub
+
+    Private Sub Yellow_Click(sender As Object, e As EventArgs) Handles Yellow.Click
+        myBrush.Color = Color.Yellow
+    End Sub
+End Class
